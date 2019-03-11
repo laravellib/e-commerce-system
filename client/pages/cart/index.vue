771aea42c229dd4c5aa6c9d88ca7243cbce063e4
Vitalii Kalaida <nevadskiy@gmail.com>
2019-03-11 23:19:36 +0200

    Fix UI when cart is empty

--- client/pages/cart/index.vue
+++ client/pages/cart/index.vue
@@ -5,11 +5,13 @@
                 <div class="column is-8">
                     <h1 class="title is-4">Your cart</h1>
 
-                    <article class="message">
+                    <article v-if="products.length" class="message">
                         <CartOverview />
                     </article>
 
-                    <a href="#" class="button is-fullwidth is-info is-medium">Checkout</a>
+                    <p v-else>Your cart is empty</p>
+
+                    <a href="#" class="button is-fullwidth is-info is-medium" :disabled="empty">Checkout</a>
                 </div>
             </div>
         </div>
@@ -17,11 +19,19 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex';
   import CartOverview from '@/components/cart/CartOverview.vue';
 
   export default {
     components: {
       CartOverview,
+    },
+
+    computed: {
+      ...mapGetters({
+        empty: 'cart/empty',
+        products: 'cart/products',
+      }),
     },
   };
 </script>
--- client/store/cart.js
+++ client/store/cart.js
@@ -1,15 +1,21 @@
 export const state = () =>  ({
   products: [],
+  empty: true,
 });
 
 export const getters = {
   products: state => state.products,
   count: state => state.products.length,
+  empty: state => state.empty,
 };
 
 export const mutations = {
   SET_PRODUCTS(state, products) {
     state.products = products;
+  },
+
+  SET_EMPTY(state, empty) {
+    state.empty = empty;
   }
 };
 
@@ -18,6 +24,7 @@ export const actions = {
     let response = await this.$axios.$get('cart');
 
     commit('SET_PRODUCTS', response.data.products);
+    commit('SET_EMPTY', response.meta.empty);
 
     return response;
   },
