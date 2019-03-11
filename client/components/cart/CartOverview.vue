a7a9ee56197d57eed4654a0f288991501cecd69b
Vitalii Kalaida <nevadskiy@gmail.com>
2019-03-11 22:50:33 +0200

    Feature cart overview page

---
+++ client/components/cart/CartOverview.vue
@@ -0,0 +1,28 @@
+<template>
+    <table class="table is-hoverable is-fullwidth">
+        <tbody>
+        <CartOverviewProduct
+                v-for="product in products"
+                :key="product.id"
+                :product="product"
+        />
+        </tbody>
+    </table>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex';
+  import CartOverviewProduct from './CartOverviewProduct.vue';
+
+  export default {
+    components: {
+      CartOverviewProduct
+    },
+
+    computed: {
+      ...mapGetters({
+        products: 'cart/products',
+      }),
+    },
+  }
+</script>
---
+++ client/pages/cart/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="section">
+        <div class="container is-fluid">
+            <div class="columns is-centered">
+                <div class="column is-8">
+                    <h1 class="title is-4">Your cart</h1>
+
+                    <article class="message">
+                        <CartOverview />
+                    </article>
+
+                    <a href="#" class="button is-fullwidth is-info is-medium">Checkout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import CartOverview from '@/components/cart/CartOverview.vue';
+
+  export default {
+    components: {
+      CartOverview,
+    },
+  };
+</script>
