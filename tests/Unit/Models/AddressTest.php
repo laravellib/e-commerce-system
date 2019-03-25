5227a75a733bdd99146cda5ba782853cc70f8d01
Vitalii Kalaida <nevadskiy@gmail.com>
2019-03-25 22:27:01 +0200

    Add address model

--- tests/Unit/Models/AddressTest.php
+++ tests/Unit/Models/AddressTest.php
@@ -4,15 +4,24 @@ namespace Tests\Unit\Models;
 
 use App\Models\Address;
 use App\Models\Country;
+use App\Models\User;
 use Tests\TestCase;
 
 class AddressTest extends TestCase
 {
     /** @test */
-    function it_belongs_to_country()
+    function it_belongs_to_a_country()
     {
         $address = factory(Address::class)->create();
 
         $this->assertInstanceOf(Country::class, $address->country);
+    }
+
+    /** @test */
+    function it_belongs_to_a_user()
+    {
+        $address = factory(Address::class)->create();
+
+        $this->assertInstanceOf(User::class, $address->user);
     }
 }
