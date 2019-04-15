4eb9aa1baa83d8270ad0ad4900e6a3e464a54317
Vitalii Kalaida <nevadskiy@gmail.com>
2019-04-15 20:03:04 +0300

    Add shipping address selector

--- client/components/checkout/addresses/ShippingAddress.vue
+++ client/components/checkout/addresses/ShippingAddress.vue
@@ -3,19 +3,44 @@
         <div class="message-body">
             <h5 class="title is-5">Ship to</h5>
 
-            <template v-if="selectedAddress">
-                {{ selectedAddress.name }} <br>
-                {{ selectedAddress.address_1 }} <br>
-                {{ selectedAddress.city }} <br>
-                {{ selectedAddress.postal_code }} <br>
-                {{ selectedAddress.country.name }}
+            <template v-if="selecting">
+                <ShippingAddressSelector
+                        :addresses="addresses"
+                        :selected-address="selectedAddress"
+                        @input="switchAddress"
+                />
+            </template>
+            <template v-else>
+                <template v-if="selectedAddress">
+                    <p>
+                        {{ selectedAddress.name }} <br>
+                        {{ selectedAddress.address_1 }} <br>
+                        {{ selectedAddress.city }} <br>
+                        {{ selectedAddress.postal_code }} <br>
+                        {{ selectedAddress.country.name }}
+                    </p>
+
+                    <br>
+                </template>
+
+                <div class="field is-grouped">
+                    <p class="control">
+                        <a href="" class="button is-info" @click.prevent="selecting = true">Change shipping address</a>
+                    </p>
+                </div>
             </template>
         </div>
     </article>
 </template>
 
 <script>
+  import ShippingAddressSelector from './ShippingAddressSelector.vue';
+
   export default {
+    components: {
+      ShippingAddressSelector,
+    },
+
     props: {
       addresses: {
         type: Array,
@@ -25,9 +50,16 @@
 
     data() {
       return {
+        selecting: false,
         localAddress: this.addresses,
         selectedAddress: null,
       };
+    },
+
+    watch: {
+      selectedAddress() {
+        this.selecting = false;
+      }
     },
 
     computed: {
