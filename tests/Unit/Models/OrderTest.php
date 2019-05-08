96fd9c0be7de1bdfb4aa0514b19882b3d4e17d6b
Vitalii Kalaida <nevadskiy@gmail.com>
2019-05-08 22:24:33 +0300

    Add products relation to orders

--- tests/Unit/Models/OrderTest.php
+++ tests/Unit/Models/OrderTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Models;
 
 use App\Models\Address;
 use App\Models\Order;
+use App\Models\ProductVariation;
 use App\Models\ShippingMethod;
 use App\Models\User;
 use Tests\TestCase;
@@ -40,5 +41,34 @@ class OrderTest extends TestCase
         $order = factory(Order::class)->create();
 
         $this->assertInstanceOf(ShippingMethod::class, $order->shippingMethod);
+    }
+
+    /** @test */
+    function it_has_many_products()
+    {
+        $order = factory(Order::class)->create();
+
+        $order->products()->attach(
+            $productVariation = factory(ProductVariation::class)->create(), [
+                'quantity' => 1
+            ]
+        );
+
+        $this->assertInstanceOf(ProductVariation::class, $order->products->first());
+        $this->assertTrue($order->products->first()->is($productVariation));
+    }
+
+    /** @test */
+    function it_has_a_quantity_attached_to_the_products()
+    {
+        $order = factory(Order::class)->create();
+
+        $order->products()->attach(
+            $productVariation = factory(ProductVariation::class)->create(), [
+                'quantity' => 2
+            ]
+        );
+
+        $this->assertEquals(2, $order->products->first()->pivot->quantity);
     }
 }
