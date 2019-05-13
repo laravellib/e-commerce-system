aaf1727ec2cdfe0d621f0cb7c1cde652cc2d02a4
Vitalii Kalaida <nevadskiy@gmail.com>
2019-05-13 22:50:47 +0300

    Setting up orders index page

--- client/pages/orders/index.vue
+++ client/pages/orders/index.vue
@@ -1,9 +1,20 @@
 <template>
     <div class="section">
         <div class="container is-fluid">
-            <div class="columns is-centered">
+            <div class="columns">
                 <div class="column is-12">
-                    <h1 class="title is-4">Orders</h1>
+                    <h1 class="title is-4">Your orders</h1>
+
+                    <article class="message">
+                        <div class="message-body">
+                            <table class="table is-hoverable is-fullwidth">
+                                <tbody>
+                                    <Order/>
+                                </tbody>
+                            </table>
+                        </div>
+                    </article>
+                    <p>You have no orders</p>
                 </div>
             </div>
         </div>
@@ -11,6 +22,11 @@
 </template>
 
 <script>
+  import Order from '@/components/orders/Order.vue';
+
   export default {
+    components: {
+      Order,
+    }
   };
 </script>
